c2597d8b6f66a29e7b58b1a34bfd92e391d9558b
Seyam <mohammadseyam248@gmail.com>
2026-08-31 15:18:56 +0600

    database connection configured successfully

---
+++ backend/utils/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const connectDB = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('mongodb connected sucessfully')
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+export default connectDB
